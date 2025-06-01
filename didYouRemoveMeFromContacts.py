7a5d07a32df60bb2ac2e0f170b581dba53b7e286
mahbubaj <mahbuba.jafarzade@gmail.com>
2025-06-01 11:57:13 +0000

    completed

---
+++ didYouRemoveMeFromContacts.py
@@ -0,0 +1,4 @@
+tlist = ["Mahbuba", "Jafar", "22", "ADA University"]
+print(tlist)
+del tlist[3]
+print(tlist)
---
+++ yesOrNo.py
@@ -0,0 +1,7 @@
+import random
+wheel = random.randint(1,2)
+if wheel == 1:
+    print("Yes")
+else:
+    print("No")
+# save
